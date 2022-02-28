b6fe57bdb6f84c45d682ceaf176d08baa6ccec75
adarshadash <57247485+adarshadash@users.noreply.github.com>
2022-02-28 16:18:19 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -5,3 +5,26 @@ def call(name){
 def anothercall(name){
    echo "Hey ${name}, Another how are you ? "
 }
+
+def _load() {
+
+  Yaml parser = new Yaml()
+  application = parser.load(("${env.WORKSPACE}/application.yaml" as File).text)
+  return application
+
+}
+
+def _save(application) {
+
+  Yaml parser = new Yaml()
+  parser.dump(application, new FileWriter("${env.WORKSPACE}/application.yaml"))
+
+}
+
+def getTag() {
+
+    newtag = _load()
+    tag = "${newtag['version']}.${newtag['build']}"
+    return tag
+
+}
