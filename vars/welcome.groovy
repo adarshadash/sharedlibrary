febdca0df44baeba635e660c36157b8458eedf9b
adarshadash <57247485+adarshadash@users.noreply.github.com>
2022-03-01 08:18:07 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -58,5 +58,5 @@ def updateApplication(){
     sh "cat ${env.WORKSPACE}/application.yaml"
     sh "echo 'The current build is: ${version}'"   
     sh "git commit -m 'increment version: ${version}'"
-    sh "git push"
+    sh "git push origin HEAD:main"
 }
