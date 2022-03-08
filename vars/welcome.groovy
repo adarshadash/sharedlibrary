4c0692c012c9d16ea058c221d77c303a88a2f67d
adarshadash <57247485+adarshadash@users.noreply.github.com>
2022-03-08 19:11:23 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -58,7 +58,7 @@ def updateApplication(){
     _save(application)
     sh "git add ${env.WORKSPACE}/application.yaml"
     sh "echo 'The current build is: ${version}'"
-    sh "git remote set-url origin git@github.com:adarshadash/sharedlibrary.git"
+    sh "git remote set-url origin https://github.com/adarshadash/sharedlibrary"
     sh "ls -la"
   /*  sh "git config --global user.email 'adi.dash880@gmail.com'"
     sh "git config --global user.name 'adarshadash'"
